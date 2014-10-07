f6e01722a115d2290f15be2b7f3f276a6795e55a
Marko Kirves <marko.kirves@bynd.com>
2014-10-07 11:13:48 +0100

    Removed tmux-powerline

--- tmux/tmux-powerline/config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Cofigurations for tmux-powerline.
-
-# You platform \in {linux,bsd,mac}.
-export PLATFORM="mac"
-
-# Useage of patched font for symbols. true or false.
-export USE_PATCHED_FONT="true"
