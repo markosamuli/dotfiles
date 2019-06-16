b56e89feb07cf0a4e5eab6e281be0cc88085c9b7
MSK <hello@msk.io>
2019-06-16 14:45:33 +0100

    fix: remove debug message

--- oh-my-zsh/oh-my-zsh.zsh
+++ oh-my-zsh/oh-my-zsh.zsh
@@ -1,8 +1,6 @@
 
 #!/bin/zsh
 
-echo "loading oh-my-zsh"
-
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
