5c6189f7fca57a001dc373548618630b9bbc1981
MSK <hello@msk.io>
2019-07-16 12:54:32 +0100

    feat: GOPATH in zsh

--- golang/path.bash
+++ golang/path.bash
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-# Golang projects
-if [ -d "$HOME/Projects/golang" ]; then
-  export GOPATH=$HOME/Projects/golang
-  export PATH=$PATH:$GOPATH/bin
+# Set GOPATH if not previously defined
+if [ -z "$GOPATH" ]; then
+  if [ -d "$HOME/go" ]; then
+    export GOPATH=$HOME/go
+    export PATH=$PATH:$GOPATH/bin
+  elif [ -d "$HOME/Projects/golang" ]; then
+    export GOPATH=$HOME/Projects/golang
+    export PATH=$PATH:$GOPATH/bin
+  fi
 fi
 
 # Load GAE tools to Golang
--- golang/path.zsh
+++ golang/path.zsh
@@ -1,5 +1,16 @@
 #!/bin/zsh
 
+# Set GOPATH if not previously defined
+if [ -z "$GOPATH" ]; then
+  if [ -d "$HOME/go" ]; then
+    export GOPATH=$HOME/go
+    export PATH=$PATH:$GOPATH/bin
+  elif [ -d "$HOME/Projects/golang" ]; then
+    export GOPATH=$HOME/Projects/golang
+    export PATH=$PATH:$GOPATH/bin
+  fi
+fi
+
 # Load GAE tools to Golang
 if [ -d "$HOME/opt/go_appengine" ]; then
   export PATH=$PATH:$HOME/opt/go_appengine
