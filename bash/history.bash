200b33eab9466d3a79f084f263b10b6620adb4ce
MSK <hello@msk.io>
2019-07-16 13:08:35 +0100

    feat: bash history config

--- bash/history.bash
+++ bash/history.bash
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+## Command history configuration
+if [ -z "$HISTFILE" ]; then
+    HISTFILE=$HOME/.bash_history
+fi
+
 # don't put duplicate lines or lines starting with space in the history.
 # See bash(1) for more options
 HISTCONTROL=ignoreboth
@@ -8,5 +13,5 @@ HISTCONTROL=ignoreboth
 shopt -s histappend
 
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
-HISTSIZE=1000
-HISTFILESIZE=2000
+HISTSIZE=10000
+SAVEHIST=10000
