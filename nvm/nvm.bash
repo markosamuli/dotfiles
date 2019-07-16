21195482be25bc88f42331ac825ea0fa3dac7ae4
MSK <hello@msk.io>
2019-07-16 12:47:36 +0100

    feat: nvm in zsh

--- nvm/nvm.bash
+++ nvm/nvm.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-# Initialise nvm if installed
-export NVM_DIR="${HOME}/.nvm"
-[ -s "${NVM_DIR}/nvm.sh" ] && . "${NVM_DIR}/nvm.sh" # This loads nvm
+# load nvm if installed
+if [ -d "$HOME/.nvm" ]; then
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh" # This loads nvm
+fi
