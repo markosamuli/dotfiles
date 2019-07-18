c76d881fe22781559f9656021d04ccb386f4561f
MSK <hello@msk.io>
2019-07-18 12:01:36 +0100

    feat: travis command completion

---
+++ travis/completion.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/env/bash
+
+# tab completion for travis
+if [ -f /home/markosamuli/.travis/travis.sh ]; then
+  source /home/markosamuli/.travis/travis.sh
+fi
---
+++ travis/completion.zsh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+# tab completion for travis
+if [ -f /home/markosamuli/.travis/travis.sh ]; then
+  source /home/markosamuli/.travis/travis.sh
+fi
