69aaba031d2de1867ae4caacf47f8506e9f5ee0a
Marko Kirves <marko.kirves@bynd.com>
2019-08-02 19:07:01 +0100

    feat: display AWS_VAULT on zsh prompt

--- aws-vault/aws-vault.bash
+++ aws-vault/aws-vault.bash
@@ -2,5 +2,7 @@
 # vim :set ts=2 sw=2 sts=2 et :
 
 if [[ "$platform" == "linux" ]]; then
-  export AWS_VAULT_BACKEND=pass
+  if [ -z "${AWS_VAULT_BACKEND}" ]; then
+    export AWS_VAULT_BACKEND=pass
+  fi
 fi
--- aws-vault/aws-vault.zsh
+++ aws-vault/aws-vault.zsh
@@ -1,5 +1,16 @@
 #!/bin/zsh
+# vim :set ts=2 sw=2 sts=2 et :
 
 if [[ "$platform" == "linux" ]]; then
-  export AWS_VAULT_BACKEND=pass
+  if [ -z "${AWS_VAULT_BACKEND}" ]; then
+    export AWS_VAULT_BACKEND=pass
+  fi
 fi
+
+aws_vault_prompt_precmd() {
+  if [[ -n "$AWS_VAULT" ]]; then
+    PROMPT="%F{cyan}${AWS_VAULT:-}%f "$PROMPT
+  fi
+}
+
+add-zsh-hook precmd aws_vault_prompt_precmd
