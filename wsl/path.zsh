aaa7300b0586a142e1fccd985fa68528af13bc5d
Marko Kirves <marko.kirves@bynd.com>
2019-08-02 17:33:44 +0100

    fix: keep C:\Windows\System32 in PATH

--- wsl/path.zsh
+++ wsl/path.zsh
@@ -1,7 +1,8 @@
 #!/bin/zsh
+# vim :set ts=2 sw=2 sts=2 et :
 
 # https://stackoverflow.com/questions/370047/what-is-the-most-elegant-way-to-remove-a-path-from-the-path-variable-in-bash
-wsl_path_remove()  {
+wsl_path_remove() {
   export PATH=`echo -n $PATH | awk -v RS=: -v ORS=: '$0 != "'$1'"' | sed 's/:$//'`
 }
 
@@ -12,10 +13,15 @@ if [[ "$platform" == "wsl" ]]; then
   typeset -U wsl_mnt_paths
   wsl_mnt_paths=("${(@f)$(echo $PATH | tr ':' '\n' | grep '^/mnt' | sort -n)}")
   for mnt_path in "${wsl_mnt_paths[@]}"; do
-    # Keep VSCode binaries in the path so we can edit files
-    if [[ ! "$mnt_path" =~ "Microsoft VS Code" ]]; then
-      wsl_path_remove "$mnt_path"
+    # Keep \Windows\System32 folder (VSCode Remote needs access to cmd.exe)
+    if [[ "$mnt_path" =~ "/Windows/[sS]ystem32$" ]]; then
+	    continue
     fi
+    # Keep VSCode binaries in the path so we can edit files
+    if [[ "$mnt_path" =~ "Microsoft VS Code" ]]; then
+	    continue
+    fi
+    wsl_path_remove "$mnt_path"
   done
 fi
 
