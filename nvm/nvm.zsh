9f0b9bd520eaea2266539780d6ebaff20f123b1b
MSK <hello@msk.io>
2019-08-03 11:16:06 +0100

    feat: .nvmrc support in zsh

--- nvm/nvm.zsh
+++ nvm/nvm.zsh
@@ -2,6 +2,33 @@
 
 # load nvm if installed
 if [ -d "$HOME/.nvm" ]; then
+
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh" # This loads nvm
+fi
+
+# call 'nvm use' automatically in a directory with a .nvmrc file
+if command -v nvm >/dev/null ; then
+
+  autoload -U add-zsh-hook
+  load-nvmrc() {
+    local node_version="$(nvm version)"
+    local nvmrc_path="$(nvm_find_nvmrc)"
+
+    if [ -n "$nvmrc_path" ]; then
+      local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+      if [ "$nvmrc_node_version" = "N/A" ]; then
+        nvm install
+      elif [ "$nvmrc_node_version" != "$node_version" ]; then
+        nvm use
+      fi
+    elif [ "$node_version" != "$(nvm version default)" ]; then
+      echo "Reverting to nvm default version"
+      nvm use default
+    fi
+  }
+  add-zsh-hook chpwd load-nvmrc
+  load-nvmrc
+
 fi
