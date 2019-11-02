572fc00ae4280eddb7175b9acace971612648ba2
MSK <hello@msk.io>
2019-11-02 19:22:13 +0000

    style: nvm/nvm.zsh

--- nvm/nvm.zsh
+++ nvm/nvm.zsh
@@ -13,12 +13,14 @@ if command -v nvm >/dev/null ; then
 
     autoload -U add-zsh-hook
     load-nvmrc() {
-        local node_version="$(nvm version)"
-        local nvmrc_path="$(nvm_find_nvmrc)"
+        local node_version
+        local nvmrc_path
+        local nvmrc_node_version
+        node_version="$(nvm version)"
+        nvmrc_path="$(nvm_find_nvmrc)"
 
         if [ -n "$nvmrc_path" ]; then
-            local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
-
+            nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
             if [ "$nvmrc_node_version" = "N/A" ]; then
                 nvm install
             elif [ "$nvmrc_node_version" != "$node_version" ]; then
