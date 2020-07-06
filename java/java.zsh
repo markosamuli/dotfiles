072b2d568b32cde5ce037485bb916bcdd23551f8
MSK <hello@msk.io>
2020-07-06 08:16:04 +0100

    fix: hide errors if Java is not installed

--- java/java.zsh
+++ java/java.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-export JAVA_HOME_14=$(/usr/libexec/java_home -v14)
+export JAVA_HOME_14=$(/usr/libexec/java_home -v14 2>/dev/null)
 if [ -n "$JAVA_HOME_14" ]; then
     export JAVA_HOME=$JAVA_HOME_14
 fi
