011cd10bbd54f50a45b1761f506cb41edb5843f1
MSK <hello@msk.io>
2020-09-09 11:08:55 +0100

    fix: use full python3 path in CLOUDSDK_PYTHON

--- gcloud/config.bash
+++ gcloud/config.bash
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
+# Use the full path so it will work even while switching between Python versions
+_full_python3_path() {
+    local python3_path
+    if command -v pyenv >/dev/null; then
+        python3_path=$(pyenv which python3)
+    fi
+    if [ -z "${python3_path}" ]; then
+        python3_path="python3"
+    fi
+    echo "${python3_path}"
+}
+
 # Use python3 over python2
 if command -v python3 >/dev/null; then
-    export CLOUDSDK_PYTHON=python3
+    CLOUDSDK_PYTHON=$(_full_python3_path)
+    export CLOUDSDK_PYTHON
 fi
