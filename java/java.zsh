ca74670b92b65bc8a19b4edee53104c3568078a7
Marko Samuli Kirves <hello@msk.io>
2022-01-06 13:24:44 +0000

    feat: use JDK 11

--- java/java.zsh
+++ java/java.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-JAVA_HOME_14=$(/usr/libexec/java_home -v14 2>/dev/null)
-if [ -n "$JAVA_HOME_14" ]; then
-    export JAVA_HOME=$JAVA_HOME_14
+JAVA_HOME_11=$(/usr/libexec/java_home -v11 2>/dev/null)
+if [ -n "$JAVA_HOME_11" ]; then
+    export JAVA_HOME=$JAVA_HOME_11
 fi
