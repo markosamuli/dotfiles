f401713b4bcbe46781e31765dbe2198ac48ace14
MSK <hello@msk.io>
2023-10-15 15:14:34 +0100

    feat: 1Password CLI

---
+++ op/op.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+if [ -e "${HOME}/.config/op/plugins.sh" ]; then
+    # shellcheck disable=SC1091
+    source "${HOME}/.config/op/plugins.sh"
+fi
---
+++ op/op.zsh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+if [ -e "${HOME}/.config/op/plugins.sh" ]; then
+    source "${HOME}/.config/op/plugins.sh"
+fi
