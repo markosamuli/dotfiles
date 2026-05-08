7abc08707f0ef479f44d2c21a6e38a86c6265d3d
MSK <hello@msk.io>
2026-05-08 15:47:27 +0100

    feat: add flutter to PATH

---
+++ flutter/path.zsh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+if [ -d "$HOME/opt/flutter/bin" ]; then
+    PATH="$HOME/opt/flutter/bin:$PATH"
+fi
+
+# Pub installs executables into $HOME/.pub-cache/bin, which is not on your path.
+if [ -d "$HOME/.pub-cache/bin" ]; then
+    PATH="$PATH:$HOME/.pub-cache/bin"
+fi
